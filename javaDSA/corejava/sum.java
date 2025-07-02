cba0143268bb131c995d0ce90384b72a7c4d545d
Nandyala Sri Manikanta Subbarao <22h45a6105@bvcits.edu.in>
2025-07-02 20:54:08 +0530

    Add sum.java for basic addition functionality with user input

---
+++ javaDSA/corejava/sum.java
@@ -0,0 +1,23 @@
+import java.util.Scanner;
+
+public class sum {
+  public static void main(String args[]){
+    // int a=10;
+    // int b=5;
+    // int sum=a+b;
+
+    // System.out.println(sum);
+
+    // comments
+
+    Scanner sc=new Scanner(System.in);
+    int a=sc.nextInt();
+    int b=sc.nextInt();
+
+    int sum=a+b;
+
+    System.out.println(sum);
+
+  }
+  
+}

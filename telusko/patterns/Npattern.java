205848db8743afd029c5c03575f67b120f77773a
Nandyala Sri Manikanta Subbarao <22h45a6105@bvcits.edu.in>
2025-11-07 19:21:55 +0530

    added learning journey of telusko patterns

---
+++ telusko/patterns/Npattern.java
@@ -0,0 +1,16 @@
+public class Npattern {
+  public static void main(String[] args) {
+    int n=5;
+    for(int i=0;i<n;i++){
+      for(int j=0;j<n;j++){
+        if(j==0 || j==n-1 || j==i){
+          System.out.print("* ");
+        }else{
+          System.out.print("  ");
+        }
+      }
+      System.out.println();
+    }
+  }
+  
+}

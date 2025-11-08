730cc9131e73e10fefc0318a7d6d3fe7818eff08
Nandyala Sri Manikanta Subbarao <22h45a6105@bvcits.edu.in>
2025-11-08 19:44:02 +0530

    added new patterns

--- telusko/patterns/Xpattern.java
+++ telusko/patterns/Xpattern.java
@@ -1,16 +1,15 @@
-public class HallowBox {
-  public static void main(String[] args){
+public class Xpattern {
+  public static void main(String[] args) {
     for(int i=0;i<5;i++){
       for(int j=0;j<5;j++){
-        if(i==0 || j==0 || i==4 || j==4){
+        if(i==j || i+j==4){
           System.out.print("* ");
-        }
-        else{
+        }else{
           System.out.print("  ");
         }
       }
       System.out.println();
     }
+    
   }
-  
 }
